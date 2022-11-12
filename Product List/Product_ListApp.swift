781bbec43c23d1905587bf705516c2ddbc62ce57
Moussaab Djeradi <fm_djeradi@esi.dz>
2022-11-12 18:02:23 +0100

    Initial Commit

---
+++ Product List/Product_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Product_ListApp.swift
+//  Product List
+//
+//  Created by Moussaab Djeradi on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Product_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
